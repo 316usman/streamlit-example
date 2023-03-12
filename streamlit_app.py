9e7b1086fca77c56fe844ea37f62cd17edbd0913
316usman <107467685+316usman@users.noreply.github.com>
2023-03-12 07:26:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,9 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-Enter Query Here Readymade
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 st.title("Ënter Query Here")
+
+query = st.st.text_input('')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
