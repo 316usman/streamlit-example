0743eaaed21e6d4b40b72c6a979c7f725544efd2
316usman <107467685+316usman@users.noreply.github.com>
2023-03-12 07:31:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ st.title("Ënter Query Here")
 
 query = st.text_input('')
 
+st.write(query)
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
